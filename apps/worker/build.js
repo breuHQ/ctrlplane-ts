2a21c35564ee63f0afdb4068d0e51b14041214bb
Yousuf Jawwad <yousuf@maher.pk>
2022-05-18 12:33:54 +0500

    chore(): updated build script

--- apps/worker/build.js
+++ apps/worker/build.js
@@ -1,5 +1,11 @@
 import { build } from 'esbuild';
 
+import { readFile } from 'fs/promises';
+
+const pkg = JSON.parse(await readFile('./package.json', 'utf8'));
+const external = [...Object.keys(pkg.dependencies || {}), ...Object.keys(pkg.peerDependencies || {})];
+console.log(external);
+
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 build({
   entryPoints: ['./src/client.ts', './src/worker.ts', './src/workflows.ts'],
@@ -8,7 +14,7 @@ build({
   // format: 'cjs',
   format: 'esm',
   platform: 'node',
-  external: ['@temporalio/*', 'rxjs/*', 'util/*', 'triple-beam/*', 'winston/*', '../../node_modules/*'],
+  external: ['@temporalio/*', ...external],
   outdir: './build',
   watch: true,
 });
